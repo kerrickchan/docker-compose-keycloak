14724d108eb37386ee31296376da93a096c742f7
Kerrick Chan <kerrickchan@icloud.com>
2023-08-06 12:46:51 +0800

    add cicd with test

---
+++ tests/compose.test.ts
@@ -0,0 +1,11 @@
+import { v2 as compose } from 'docker-compose';
+import { logOutput, isContainerRunning } from './helper';
+
+describe('when upAll is called', () => {
+  it('should successful run', async () => {
+    await compose.upAll({ cwd: __dirname, log: logOutput })
+    expect(await isContainerRunning('/keycloak-keycloak-postgres-1')).toBeTruthy()
+    expect(await isContainerRunning('/keycloak-keycloak-1')).toBeTruthy()
+    await compose.down({ cwd: __dirname, log: logOutput })
+  }, 30000);
+});
